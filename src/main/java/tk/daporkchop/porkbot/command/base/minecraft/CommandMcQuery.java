d2b847aa0711be52564ec933eb05b0b6e55a7a6f
DaMatrix <joey.rabil@gmail.com>
2017-03-28 07:19:32 +0200

    Typo

--- src/main/java/tk/daporkchop/porkbot/command/base/minecraft/CommandMcQuery.java
+++ src/main/java/tk/daporkchop/porkbot/command/base/minecraft/CommandMcQuery.java
@@ -83,7 +83,7 @@ public class CommandMcQuery extends Command {
 
         if (!query.get("status").getAsBoolean())    {
             if (ping.get("status").getAsBoolean())  {
-                EmbdBuilder builder = new EmbedBuilder();
+                EmbedBuilder builder = new EmbedBuilder();
                 builder.setColor(Color.ORANGE);
                 builder.addField("**Unable to query**", "The server `" + args[1] + "` is online, but we were unable to query it. Make sure that `enable-query` is set to `true` in `server.properties` and that the server's port is open on UDP!", false);
                 
